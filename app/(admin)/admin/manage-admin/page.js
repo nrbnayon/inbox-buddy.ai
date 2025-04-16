8dd8bf0d6234befeadf7815d48ae5ae42e125ca5
Badhan Samadder <badhansamadder5@gmail.com>
2025-04-16 14:46:24 +0600

    manage admin page design and user action implemented

--- app/(admin)/admin/manage-admin/page.js
+++ app/(admin)/admin/manage-admin/page.js
@@ -1,5 +1,13 @@
-import React from "react";
-
+import { RiAdminFill } from "react-icons/ri";
+import AdminListTable from "./components/AdminListTable";
 export default function manageAdminPage() {
-  return <div>Manage Admins page</div>;
+  return (
+    <div>
+      <h2 className="text-2xl ml-6 mt-10 mb-5 font-bold flex gap-2">
+        <RiAdminFill size={30} />
+        Admins List:
+      </h2>
+      <AdminListTable />
+    </div>
+  );
 }
