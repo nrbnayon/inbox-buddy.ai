a922461394df7e7e9f54b5a37712703c144bac72
Badhan Samadder <badhansamadder5@gmail.com>
2025-04-18 08:10:58 +0600

    icon color change on admin dashboard page

--- next.config.mjs
+++ next.config.mjs
@@ -27,16 +27,16 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_API_BASE_URL:
-      process.env.NODE_ENV === "production"
-        ? "https://ai-chat-bot-assistant-server.vercel.app/api/v1"
-        : "http://192.168.10.32:4000/api/v1",
-    API_BASE_URL:
-      process.env.NODE_ENV === "production"
-        ? "https://ai-chat-bot-assistant-server.vercel.app/api/v1"
-        : "http://192.168.10.32:4000/api/v1",
-  },
+  // env: {
+  //   NEXT_PUBLIC_API_BASE_URL:
+  //     process.env.NODE_ENV === "production"
+  //       ? "https://ai-chat-bot-assistant-server.vercel.app/api/v1"
+  //       : "http://192.168.10.32:4000/api/v1",
+  //   API_BASE_URL:
+  //     process.env.NODE_ENV === "production"
+  //       ? "https://ai-chat-bot-assistant-server.vercel.app/api/v1"
+  //       : "http://192.168.10.32:4000/api/v1",
+  // },
   devIndicators: false,
 };
 
